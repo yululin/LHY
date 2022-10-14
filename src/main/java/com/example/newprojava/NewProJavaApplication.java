83e061edf20325e376c72302a8b0f779461275f7
a1739 <a17398823621@163.com>
2022-10-14 14:07:08 +0800

    11

---
+++ src/main/java/com/example/newprojava/NewProJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.newprojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewProJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewProJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/newprojava/NewProJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.newprojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewProJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
